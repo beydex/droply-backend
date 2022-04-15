161471939bb3fbeaf61a9c4a6e0410dafb6f6cf8
Булат <bularond@gmail.com>
2022-04-15 21:21:01 +0000

    DROP-69: Убрал поле email из contact/list сцены

--- droply-data/src/main/kotlin/ru/droply/data/common/dto/DroplyUserContactOutDto.kt
+++ droply-data/src/main/kotlin/ru/droply/data/common/dto/DroplyUserContactOutDto.kt
@@ -3,4 +3,4 @@ package ru.droply.data.common.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class DroplyUserContactOutDto(val id: Long, val name: String, val email: String, val avatarUrl: String? = null)
+data class DroplyUserContactOutDto(val id: Long, val name: String, val avatarUrl: String? = null)
